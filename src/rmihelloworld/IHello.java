2bdb437bb891c75b41aeac71011e6f7e3ae0beb1
Dany <ifeifei@stu.xmu.edu.cn>
2016-02-26 09:32:50 +0800

    rmi

---
+++ src/rmihelloworld/IHello.java
@@ -0,0 +1,10 @@
+package rmihelloworld;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IHello extends Remote
+{
+    public String helloWorld() throws RemoteException; 
+    public String sayHelloToSomeBody(String someBodyName) throws RemoteException; 
+}
